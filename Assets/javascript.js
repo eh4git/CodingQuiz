bb3389ff741cff4d4ed13bc2934f8d7b52c99f81
erik hirsch <ehirsch760@gmail.com>
2020-04-26 23:37:54 -0700

    Timer partial, question array, first question load

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,30 +1,102 @@
-var timerEl = document.getElementById("time");
 var quizDisplay = document.getElementById("quizArea");
 
 
-var quizQuestion1={
-    quizDisplay=""
+timeOnClock = 5;
+function startQuiz() {
+    // var timerStop =  setInterval(setTime, 1000);
+    questionSelection();
+    questIndex++;
+    //         console.log("Timer: "+ timeOnClock);
+};
+var timerStop = setInterval(setTime, 1000);
+function setTime() {
+    timeOnClock--;
+    var timerEl = document.querySelector("#time");
+
+    timerEl.textContent = timeOnClock;
+    console.log(timeOnClock)
+
+    if (timeOnClock === 0) {
+        clearInterval(timerStop);
+
+    }
+}
+
+var questArray = [
+    {
+        question: "Whats my fav color?",
+        answer: ["a", "b", "c", "d"],
+        correctAns: "d"
+    },
+    {
+        question: "Whats your fav color?",
+        answer: ["green", "blue", "red", "orange"],
+        correctAns: "blue"
+    },
+    {
+        question: "Whats his fav color?",
+        answer: ["green", "blue", "red", "orange"],
+        correctAns: "green"
+    },
+    {
+        question: "Whats her fav color?",
+        answer: ["green", "blue", "red", "orange"],
+        correctAns: "orange"
+    },
+    {
+        question: "Whats their fav color?",
+        answer: ["green", "blue", "red", "orange"],
+        correctAns: "red"
+    },
+    {
+        question: "Whats its fav color?",
+        answer: ["green", "blue", "red", "orange"],
+        correctAns: "blue"
+    },
+    {
+        question: "Whats our fav color?",
+        answer: ["green", "blue", "red", "orange"],
+        correctAns: "green"
+    },
+    {
+        question: "Whats everyone's fav color?",
+        answer: ["green", "blue", "red", "orange"],
+        correctAns: "red"
+    },
+]
+var questIndex = 0;
+
+
+
+function questionSelection() {
+    var questEl = document.querySelector("#quizQuestion");
+    questEl.textContent = questArray[questIndex].question
+    var answer1El = document.querySelector("#answer1")
+    answer1El.textContent = questArray[questIndex].answer[0]
+    var answer2El = document.querySelector("#answer2")
+    answer2El.textContent = questArray[questIndex].answer[1]
+    var answer3El = document.querySelector("#answer3")
+    answer3El.textContent = questArray[questIndex].answer[2]
+    var answer4El = document.querySelector("#answer4")
+    answer4El.textContent = questArray[questIndex].answer[3]
+    allAnswers = document.querySelectorAll("#answer")
+    
+    var rightAns = questArray[questIndex].correctAns
+    for (i = 0; i > allAnswers.lenght; i++) {
+        var answerTxt = allAnswers[i].textContent;
+        if (answerTxt === rightAns) {
+
+        }
+    }
+
 }
 
 
-// timeLeft = 75;
-// for(i=0; i > 0; i++){
-//     setInterval(1000);
-//     timerEl--;
-//     console.log("Timer: "+ timer)
+var startBtn = document.querySelector(".start");
+startBtn.addEventListener("click", startQuiz);
+//   for(i=timeOnClock; i > 0; i--){setInterval(runTimer,1000);{
+//          timeOnClock--;
+//         var timerEl = document.querySelector("#time");
+//         timerEl.textContent = timeOnClock;
+//     }
 // }
-
-function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timerEl.textContent = timeLeft;
-      console.log(timeLeft)
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        sendMessage();
-      }
-  
-    }, 1000);
-  }
-  setTime;
